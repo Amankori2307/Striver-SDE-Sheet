aeb309a2b88f21a2755128da05646873c664361c
Aman <aman@Amans-MBP-2.bbrouter>
2022-08-09 07:13:31 +0530

    Wrote 2nd approach for set matrix zero

--- 1.cpp
+++ 1.cpp
@@ -41,6 +41,38 @@ class Solution{
             }
             disp(arr, n);
         }
+
+        void approach2(vector<vector<int>> matrix, int n){
+            int col0 = 1;
+            for (int i = 0; i < n; i++) {
+                //checking if 0 is present in the 0th column or not
+                if (matrix[i][0] == 0) col0 = 0;
+                for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+                }
+            }
+            //traversing in the reverse direction and
+            //checking if the row or col has 0 or not
+            //and setting values of matrix accordingly.
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+                }
+                if (col0 == 0) {
+                matrix[i][0] = 0;
+                }
+
+            }
+
+
+            disp(matrix, n);
+
+        }
 };
 
 
@@ -55,6 +87,6 @@ int main(){
     }
 
     Solution s;
-    s.approach1(arr, n);
+    s.approach2(arr, n);
     return 0;
 }
